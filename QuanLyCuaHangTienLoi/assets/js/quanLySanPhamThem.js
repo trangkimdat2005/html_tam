27d1592b120fff066db13cbe30a066548c809e5c
ngominhkhoi05 <khoi22092021@gmail.com>
2025-11-04 19:06:02 +0700

    sua lai cau truc

---
+++ QuanLyCuaHangTienLoi/assets/js/quanLySanPhamThem.js
@@ -0,0 +1,16 @@
+
+    $(document).ready(function () {
+      $('#category').select2({
+        placeholder: 'Chọn loại hàng hóa...',
+        allowClear: true
+      });
+    });
+
+
+
+    $(document).ready(function () {
+      $('#promotion').select2({
+        placeholder: 'Chọn loại khuyến mãi...',
+        allowClear: true
+      });
+    });
